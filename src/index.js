825055b82191382ee6b0b784ffd6b588028168cc
Weslley Araujo <weslleyaraujo20@gmail.com>
2018-03-16 14:24:12 +0100

    fix(*): pass store into App render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // @flow
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import App from './App';
+import App from "./App";
+import store from "./store";
 
-const root: HTMLElement = global.window.document.getElementById('root')
+const root: HTMLElement = global.window.document.getElementById("root");
 
-render(<App />, root);
+render(<App store={store} />, root);
