3a719a7558a7a5c40afe18e07e5d9961629f5f7a
Weslley Araujo <weslleyaraujo20@gmail.com>
2018-03-16 14:59:34 +0100

    gh-pages update

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import store from "./store";
-import App from "./App";
-
-it("renders App correctly", () => {
-  const wrapper = shallow(<App store={store} />);
-  expect(toJson(wrapper)).toMatchSnapshot();
-});
